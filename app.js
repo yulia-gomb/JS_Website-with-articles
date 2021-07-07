844546e25f003c5f7f1581c80e3f96ca1dac4c5f
unknown <juliesg@tut.by>
2021-07-07 16:45:37 +0300

    added articles

--- app.js
+++ app.js
@@ -8,15 +8,57 @@ window.onload = function () {
                 id: 1,
                 title: 'High quality',
                 text: 'Straightforward APIs with consistent cross platform behaviour..',
+                url: 'img/article1.png'
             },
             {
                 id: 2,
                 title: 'How to RxJS in Angular',
-                text: 'As the values of your component inputs change over time, you may want to do something ' +
-                    'with that data inside your component.',
-            }
+                text: 'As the values of your component inputs change over time, you may want to do something with that data inside your component.',
+                url: 'img/article2.png'
+            },
+            {
+                id: 3,
+                title: 'Top 15 Features of Angular',
+                text: 'It has added the information for the dependency and also regarding the ng-content selections for the data.',
+                url: 'img/article3.png'
+            },
+            {
+                id: 4,
+                title: 'Angular team streamlines \n' +
+                    'feature requests',
+                text: 'Feature requests will be reviewed for alignment with existing projects on the Angular roadmap.',
+                url: 'img/article4.png'
+            },
+            {
+                id: 5,
+                title: 'How to RxJS in Angular',
+                text: 'As the values of your component inputs change over time, you may want to do something with that data inside your component.',
+                url: 'img/article2.png'
+            },
+            {
+                id: 6,
+                title: 'Top 15 Features of Angular ',
+                text: 'It has added the information for the dependency and also regarding the ng-content selections for the data.',
+                url: 'img/article3.png'
+            },
+            {
+                id: 7,
+                title: 'Angular team streamlines \n' +
+                    'feature requests',
+                text: 'Feature requests will be reviewed for alignment with existing projects on the Angular roadmap.',
+                url: 'img/article4.png'
+            },
+            {
+                id: 8,
+                title: 'High quality',
+                text: 'Straightforward APIs with consistent cross platform behaviour..',
+                url: 'img/article1.png'
+            },
         ]
     }
+
+    //добавление тегов на страницу
+
     let tags = document.getElementById('tags');
 
     data.tags.forEach(function (item, i){
@@ -29,10 +71,27 @@ window.onload = function () {
         }
     )
 
+    //добавление артикулов на страницу
+
+    let arts = document.getElementById('articles');
+
+    console.log(data.articles)
+
+    data.articles.forEach(function (item, i) {
+        let frame = ce("div","", "frame")
+        arts.append(frame)
+        console.log(item, i)
+        let framePlace = document.getElementsByClassName("frame")[i]
+        let artImage = ce("img","", "articleImage");
+        artImage.setAttribute("src", item.url)
+        console.log(artImage)
+        let artTitle = ce("div", item.title, "articleTitle")
+        let artText = ce("div", item.text, "articleText")
+        framePlace.append(artImage, artTitle, artText)
+    })
 
 
-    let ul = ce("ul");
-    document.body.append(ul);
+    //
 
 
 
