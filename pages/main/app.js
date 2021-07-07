d6542973452e5056b818ed06bda8f7edc32154fd
unknown <juliesg@tut.by>
2021-07-07 22:52:22 +0300

    article page finished

--- pages/main/app.js
+++ pages/main/app.js
@@ -1,62 +1,5 @@
 window.onload = function () {
 
-    const data = {
-        tags: ['Angular', 'Design', 'SAP ABAP', 'Product Development', 'Web Disign', 'SAP TM Consultant',
-            'DevOps', 'UX/UI Design', 'Android', 'Frontend', 'Java ', 'Programmer', 'Python'],
-        articles: [
-            {
-                id: 1,
-                title: 'High quality',
-                text: 'Straightforward APIs with consistent cross platform behaviour..',
-                url: 'img/article1.png'
-            },
-            {
-                id: 2,
-                title: 'How to RxJS in Angular',
-                text: 'As the values of your component inputs change over time, you may want to do something with that data inside your component.',
-                url: 'img/article2.png'
-            },
-            {
-                id: 3,
-                title: 'Top 15 Features of Angular',
-                text: 'It has added the information for the dependency and also regarding the ng-content selections for the data.',
-                url: 'img/article3.png'
-            },
-            {
-                id: 4,
-                title: 'Angular team streamlines \n' +
-                    'feature requests',
-                text: 'Feature requests will be reviewed for alignment with existing projects on the Angular roadmap.',
-                url: 'img/article4.png'
-            },
-            {
-                id: 5,
-                title: 'How to RxJS in Angular',
-                text: 'As the values of your component inputs change over time, you may want to do something with that data inside your component.',
-                url: 'img/article2.png'
-            },
-            {
-                id: 6,
-                title: 'Top 15 Features of Angular ',
-                text: 'It has added the information for the dependency and also regarding the ng-content selections for the data.',
-                url: 'img/article3.png'
-            },
-            {
-                id: 7,
-                title: 'Angular team streamlines \n' +
-                    'feature requests',
-                text: 'Feature requests will be reviewed for alignment with existing projects on the Angular roadmap.',
-                url: 'img/article4.png'
-            },
-            {
-                id: 8,
-                title: 'High quality',
-                text: 'Straightforward APIs with consistent cross platform behaviour..',
-                url: 'img/article1.png'
-            },
-        ]
-    }
-
     //добавление тегов на страницу
 
     let tags = document.getElementById('tags');
@@ -66,8 +9,9 @@ window.onload = function () {
             let tag = ce("li", item, "active");
             tags.append(tag);
         }
-        else {let tag = ce("li", item, "tag");
-        tags.append(tag);}
+        else {
+            let tag = ce("li", item, "tag");
+            tags.append(tag);}
         }
     )
 
@@ -98,9 +42,7 @@ window.onload = function () {
     })
 
 
-    //
-
-
+    //убрать
 
     const addLi = () =>{
         const liClick = (e) =>{
@@ -130,12 +72,15 @@ window.onload = function () {
 
     }
 
-    /* tag.addEventListener("click",addLi); */
-    //Добавление обработчика события (фильтрация статей по тегу при нажатии на кнопку)
+    //Добавление обработчика события
+
+    tag.addEventListener("click",addLi);
+
 
 
 }
 
+// функция создания элементов
 
 function ce(name,text,className,event,fn) {
     let element = document.createElement(name);
